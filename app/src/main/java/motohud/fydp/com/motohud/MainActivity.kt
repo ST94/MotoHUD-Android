e5420aa876a2ef29d932c97f7682743efc1694ef
Shing Truong <s24truon@uwaterloo.ca>
2018-02-06 03:33:34 -0500

    Initial commit with google maps api key

---
+++ app/src/main/java/motohud/fydp/com/motohud/MainActivity.kt
@@ -0,0 +1,17 @@
+package motohud.fydp.com.motohud
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import motohud.fydp.com.motohud.maps.MapsActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+}
